29dee34fba0922968018187f31ee2360b42d1bf2
ChesnovAE <aechesnov@yandex.ru>
2020-05-02 21:17:36 +0300

    Add choose location screen

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -9,6 +10,19 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   @override
   Widget build(BuildContext context) {
+
+    List<WorldTime> locations = [
+      WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+      WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+      WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+      WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+      WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+      WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+      WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+      WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+      WorldTime(url: 'Europe/Moscow', location: 'Moscow', flag: 'russia.jpg')
+    ];
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       /**
@@ -23,7 +37,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('Choose location screen'),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  print(locations[index].location);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
